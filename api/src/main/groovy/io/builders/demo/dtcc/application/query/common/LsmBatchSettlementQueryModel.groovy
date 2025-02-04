e20e3f73e64114988baba0dbc6c0a183eb0be1bf
carlos <carlos.ramon@io.builders>
2025-02-04 16:15:11 +0100

    Add get batches endpoint
    Signed-off-by: carlos <carlos.ramon@io.builders>

---
+++ api/src/main/groovy/io/builders/demo/dtcc/application/query/common/LsmBatchSettlementQueryModel.groovy
@@ -0,0 +1,13 @@
+package io.builders.demo.dtcc.application.query.common
+
+import java.time.OffsetDateTime
+
+class LsmBatchSettlementQueryModel {
+    Integer id
+    String buyerAlias
+    String sellerAlias
+    String status
+    BigDecimal securityAmount
+    BigDecimal cashAmount
+    OffsetDateTime creationDate
+}
---
+++ api/src/main/groovy/io/builders/demo/dtcc/application/query/getbatches/GetLsmBatchesQuery.groovy
@@ -0,0 +1,7 @@
+package io.builders.demo.dtcc.application.query.getbatches
+
+import io.builders.demo.core.query.Query
+import io.builders.demo.dtcc.application.query.common.GetLsmBatchesQueryModel
+
+class GetLsmBatchesQuery extends Query<List<GetLsmBatchesQueryModel>> {
+}
