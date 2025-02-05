37ce58d842f002155d1755dc6c39341adaf1e738
Miguel Gomez Cuesta <miguel@io.builders>
2025-02-05 11:09:28 +0100

    fix duplicated settlements

--- api/src/main/groovy/io/builders/demo/dtcc/infrastructure/listener/lsm/calculateoperations/CalculateLsmOperationsPort.groovy
+++ api/src/main/groovy/io/builders/demo/dtcc/infrastructure/listener/lsm/calculateoperations/CalculateLsmOperationsPort.groovy
@@ -1,7 +1,5 @@
 package io.builders.demo.dtcc.infrastructure.listener.lsm.calculateoperations
 
-import io.builders.demo.dtcc.domain.lsmbatch.event.LsmNetCalculatedEvent
 
-
-class CalculateLsmOperations extends EventListener<LsmNetCalculatedEvent> {
+class CalculateLsmOperationsPort {
 }
