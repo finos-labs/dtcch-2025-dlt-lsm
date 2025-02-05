785d23b064a1853873aae375b1be27ed81453015
carlos <carlos.ramon@io.builders>
2025-02-05 13:19:36 +0100

    fix codenarc
    Signed-off-by: carlos <carlos.ramon@io.builders>

--- api/src/main/groovy/io/builders/demo/dtcc/application/command/updatesettlements/UpdateSettlementsCommandHandler.groovy
+++ api/src/main/groovy/io/builders/demo/dtcc/application/command/updatesettlements/UpdateSettlementsCommandHandler.groovy
@@ -4,16 +4,12 @@ import io.builders.demo.core.command.CommandHandler
 import io.builders.demo.core.event.EventBus
 import io.builders.demo.dtcc.domain.lsmbatch.LsmBatch
 import io.builders.demo.dtcc.domain.lsmbatch.service.CheckLsmBatchExistsDomainService
-import io.builders.demo.dtcc.domain.settlement.Settlement
 import io.builders.demo.dtcc.domain.settlement.SettlementRepository
 import io.builders.demo.dtcc.domain.settlement.SettlementStatus
-import io.builders.demo.dtcc.domain.settlement.event.SettlementCreatedEvent
 import io.builders.demo.dtcc.domain.settlement.event.SettlementsUpdatedEvent
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-
-import java.time.OffsetDateTime
 
 @Component
 class UpdateSettlementsCommandHandler implements CommandHandler<SettlementsUpdatedEvent, UpdateSettlementsCommand> {
