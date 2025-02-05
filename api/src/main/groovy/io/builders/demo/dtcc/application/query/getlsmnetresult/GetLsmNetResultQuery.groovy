25dbf8d10f97d10c799e909b794b0169ad21644c
sergio_alba <alcalde@io.builders>
2025-02-05 12:46:31 +0100

    Small fixes #2
    Signed-off-by: sergio_alba <alcalde@io.builders>

--- api/src/main/groovy/io/builders/demo/dtcc/application/query/getlsmnetresult/GetLsmNetResultQuery.groovy
+++ api/src/main/groovy/io/builders/demo/dtcc/application/query/getlsmnetresult/GetLsmNetResultQuery.groovy
@@ -1,10 +1,11 @@
 package io.builders.demo.dtcc.application.query.getlsmnetresult
 
 import io.builders.demo.core.query.Query
+import io.builders.demo.dtcc.application.query.common.GetAiCombinationQueryModel
 import io.builders.demo.integration.model.Balance
 import io.builders.demo.integration.model.IASettlement
 
-class GetLsmNetResultQuery extends Query<List<IASettlement>> {
+class GetLsmNetResultQuery extends Query<GetAiCombinationQueryModel> {
 
     List<IASettlement> settlements
     Map<String, Balance> balances
